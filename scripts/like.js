b7492e5211990b3dae04ddfa6da302e33cef450a
V Kuznetsov <vvsmith13@gmail.com>
2025-01-09 22:11:14 +0300

    r1.04_step-2.3

--- scripts/like.js
+++ scripts/like.js
@@ -2,15 +2,20 @@
 ✦ like-icon — для svg-иконки анимированного сердца
 ✦ card__like-button — для кнопки Like рядом с иконкой
 ✦ card__icon-button — для кнопки, оборачивающей иконку
-✦ card__icon-button — для кнопки, оборачивающей иконку
 ✦ is-liked — для обозначения состояния лайкнутой иконки в виде сердца
 ✦ button__text — для обозначения текстового элемента внутри кнопки
 Если эти классы поменять в HTML, скрипт перестанет работать. Будьте аккуратны.
 */
 
+/* Индексы соответствующих HTML-элементов (иконок, кнопок) карточки во 
+всех масивах совпадают */
+
 const likeHeartArray = document.querySelectorAll('.like-icon');
 const likeButtonArray = document.querySelectorAll('.card__like-button');
 const iconButtonArray = document.querySelectorAll('.card__icon-button');
+
+/* Назначение toggleIsLiked() всем кнопкам с иконками (iconButtonArray[]) 
+и текстом "Like" (likeButtonArray[]) в качестве обработчика события onclick  */
 
 iconButtonArray.forEach((iconButton, index) => {
   iconButton.onclick = () =>
@@ -21,10 +26,16 @@ likeButtonArray.forEach((button, index) => {
   button.onclick = () => toggleIsLiked(likeHeartArray[index], button);
 });
 
+/* toggle .is-liked в массиве иконок (likeHeartArray[]) с последующим 
+запуском setButtonText() */
+
 function toggleIsLiked(heart, button) {
   heart.classList.toggle('is-liked');
   setButtonText(heart, button);
 }
+
+/* Изменение текста кнопок ( Like <=> Unlike) при наличии / отсутствии
+.is-liked с timeout 500ms */
 
 function setButtonText(heart, button) {
   if ([...heart.classList].includes('is-liked')) {
